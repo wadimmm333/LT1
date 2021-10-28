23e6675477e6abcab27e11507d932bbe57a5b5b0
user <mparinos56@gmail.com>
2021-10-28 11:03:02 +0300

    Без коммента

---
+++ letuchka1/letuchka1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace letuchka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
